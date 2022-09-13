0fe35e813eff2ad378593ccea8633d36902c5c1f
bakuleva <t.kovantseva@mail.ru>
2022-09-13 18:08:47 +0700

    ДЗ8

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,52 @@
 public class Main {
     public static void main(String[] args) {
 
-        int[] weights=new int[]{90,91,23,45,56,12,12,23,34,45,56,0};
+        /*int[] weights=new int[]{90,91,23,45,56,12,12,23,34,45,56,0};
         weights[0]=90;
         int januaryWeights=weights[0];
         System.out.println(januaryWeights);
         System.out.println(weights[0]);
         System.out.println(weights[4]);
         for (int i = 0; i < weights.length; i++) {
-            System.out.println(weights[i]);
+            System.out.println(weights[i]);*/
+        /*int[] a = new int[]{1, 2, 3};
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + ",");
 
-        }
+        System.out.println("");
+
+        double[] b = new double[]{1, 7, 9};
+        for (int j = 0; j < b.length; j++)
+            System.out.print(b[j] + " , ");
+
+        System.out.println("");
+
+        long[] c = new long[]{34, 9087, 56, 789};
+        for (int p = 0; p < c.length; p++)
+            System.out.print(c[p] + " , ");
+
+        System.out.println("");*/
+
+        int[] a1 = new int[]{1, 2, 3};
+        for (int i1 = a1.length-1; i1 >= 0; i1--)
+            System.out.print(a1[i1] + ",");
+
+        System.out.println("");
+        double[] b1 = new double[]{1, 7, 9};
+        for (int j1 = b1.length-1; j1 >=0; j1--)
+            System.out.print(b1[j1] + " , ");
+
+        System.out.println("");
+        long[] c1 = new long[]{34, 9087, 56, 789};
+        for (int p1 = c1.length-1; p1 >=0; p1--)
+            System.out.print(c1[p1] + " , ");
+        System.out.println("");
+        int[] a2 = new int[]{1, 2, 3,4,5,6,7,8};
+        for (int i2 = 0; i2 < a2.length; i2++){if(a2[i2]%2!=0)
+
+            System.out.print(a2[i2] +1+ ",");}
+
+
     }
 }
+
